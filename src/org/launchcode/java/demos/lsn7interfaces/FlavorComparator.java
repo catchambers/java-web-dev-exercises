ee7d614e44f6a48a18427dc3d2a67998380ab85c
CAT CHAMBERS <caitlynlchambers@gmail.com>
2020-09-13 21:52:10 -0500

    Adding ConeComparator Class

--- src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
@@ -7,6 +7,18 @@ public class FlavorComparator implements Comparator<Flavor> {
     @Override
     public int compare(Flavor o1, Flavor o2) {
         //returns object based on what Flavor object name comes first, alphabetically
-        return o1.getName().compareTo(o2.getName());
+//        return o1.getName().compareTo(o2.getName());
+
+        //sort objects by number of allergens from highest to lowest
+        int o1NumberAllergens = o1.getAllergens().size();
+        int o2NumberAllergens = o2.getAllergens().size();
+
+        if (o1NumberAllergens > o2NumberAllergens) {
+           return -1;
+        } else if (o1NumberAllergens < o2NumberAllergens) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
